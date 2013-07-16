cbe82d9270796886e7243f68a904a48d58727b29
John Peebles <jpeeblesg@gmail.com>
2013-07-15 20:53:43 -0400

    -empty-

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 CIic2513::Application.routes.draw do
   	
-  resources :cards
-
-
-  	get "log_out" => "sessions#destroy", :as => "log_out"
-  	get "log_in" => "sessions#new", :as => "log_in"
-  	get "sign_up" => "users#new", :as => "sign_up"
-
-  	resources :sessions
-  	resources :users
 
   	get "home" => "home#index", :as => "home"
   	root :to => "home#index"
@@ -17,7 +8,4 @@ CIic2513::Application.routes.draw do
 
 
   	#para llamar a las rutas en el servidor: /rails/routes
-  	mount_sextant if Rails.env.development?
-	match '*not_found' => 'errors#handle404'
-
 end
