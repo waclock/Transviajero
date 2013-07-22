9e87f8575bd3e28fa969abfda8836d1946273aa6
John Peebles <jpeeblesg@gmail.com>
2013-07-22 18:25:45 -0400

    cachely

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
 end
+
+gem 'rack-cachely'
 gem 'newrelic_rpm'
 gem 'jquery-rails'
 gem "thin"
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
-  
+ 
+ before_filter :cache_loading, :only => [:index]
+
+ def cache_loading
+  headers['Cache-Control'] = 'public; max-age=86400'
+end
+
   def index
   	@trip=Trip.find(:all)
   end
