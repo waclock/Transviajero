b068673fbb5a73c57435cb472d7220231b2f9fcf
John Peebles <jpeeblesg@gmail.com>
2013-07-24 12:11:57 -0400

    -empty-

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module CIic2513
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
+    config.assets.precompile += %w( bootstrap.css )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 CIic2513::Application.initialize!
-config.assets.precompile += %w( bootstrap.css )
